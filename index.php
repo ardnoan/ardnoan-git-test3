ae593be156e7780748631c1217fce1707a3f4ae8
Ardnoan <ardin218120065@gmail.com>
2023-09-20 10:33:20 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remotto</title>
+</head>
+
+<body>
+    <h1>TEss tos</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque tenetur amet, vitae corporis assumenda perspiciatis rerum veritatis sequi omnis, optio aspernatur, numquam reprehenderit porro esse eveniet quas labore dolore voluptas totam ea soluta inventore animi quod officia? Deserunt, distinctio perferendis. Repellat cupiditate esse voluptate magni consequatur quo? Alias, et nobis a deserunt saepe atque temporibus optio itaque sunt, voluptatum debitis consequuntur ducimus in molestiae! Sunt dicta alias aliquid excepturi libero atque doloribus modi voluptates, quo minus sint nobis deserunt, illum, sed explicabo consectetur quasi animi dolorum nesciunt molestias beatae deleniti eveniet. Quod tempore quia, consectetur quam qui voluptatem est corrupti?</p>
+</body>
+
+</html>
