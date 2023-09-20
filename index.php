5eb085d357b925f5cb9c80790824ef64d4e8d94f
Ardnoan <ardin218120065@gmail.com>
2023-09-20 10:43:39 +0700

    menambahkan paragraf ke-3

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <h1>TEss tos</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque tenetur amet, vitae corporis assumenda perspiciatis rerum veritatis sequi omnis, optio aspernatur, numquam reprehenderit porro esse eveniet quas labore dolore voluptas totam ea soluta inventore animi quod officia? Deserunt, distinctio perferendis. Repellat cupiditate esse voluptate magni consequatur quo? Alias, et nobis a deserunt saepe atque temporibus optio itaque sunt, voluptatum debitis consequuntur ducimus in molestiae! Sunt dicta alias aliquid excepturi libero atque doloribus modi voluptates, quo minus sint nobis deserunt, illum, sed explicabo consectetur quasi animi dolorum nesciunt molestias beatae deleniti eveniet. Quod tempore quia, consectetur quam qui voluptatem est corrupti?</p>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem accusamus explicabo assumenda facere ea porro inventore. Corrupti sequi accusamus dicta rem vitae pariatur cupiditate provident molestiae omnis repellendus ad sapiente, debitis praesentium error? Sit facilis ullam natus accusamus veniam voluptatum asperiores voluptas, eius ut quasi modi exercitationem neque perspiciatis quae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi praesentium nihil laborum explicabo dolorem laboriosam at harum alias placeat beatae, incidunt architecto. Vitae, deleniti aperiam?</p>
 </body>
 
 </html>
